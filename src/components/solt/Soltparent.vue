f57779f77f4f0dd4466fb8f4629889c2d27e188f
linhu <1205668006@qq.com>
2020-08-31 08:28:39 +0800

    换工程文件

---
+++ src/components/solt/Soltparent.vue
@@ -0,0 +1,13 @@
+<template>
+	<div>
+		<slot name="a"></slot>
+	</div>
+</template>
+
+<script>
+	export default{
+	}
+</script>
+
+<style>
+</style>
---
+++ src/components/solt/Sonsolt.vue
@@ -0,0 +1,20 @@
+<template>
+	<div>
+		<Soltparent>
+			<span slot="a">aaaaaaaaaa</span>
+		</Soltparent>
+		
+	</div>
+</template>
+
+<script>
+	import Soltparent from 'Soltparent.vue'
+	export default{
+		components:{
+			Soltparent
+		}
+	}
+</script>
+
+<style>
+</style>
